009525705f381bb08f38cea104f86267abd14e6d
iskanderandrew <iskander.s.andrews@gmail.com>
2021-02-24 10:11:12 +0200

    blockchain/blockchain.go: Refactoring initBlockchain func to check first if there is a blockchain in the Db to return the lastHash, and if not we will create a new Blockchain instance with the Genesis() block.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"fmt"
 	"github.com/dgraph-io/badger"
 )
 
@@ -20,6 +21,41 @@ func (chain *Blockchain) AddBlock(data string) {
 }
 
 func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
+	var lastHash []byte
+
+	opts := badger.DefaultOptions(dbPath)
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handler(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handler(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handler(err)
+			err = item.Value(func(val []byte) error {
+				lastHash = val
+
+				return err
+			})
+			return err
+		}
+	})
+	Handler(err)
+
+	blockchain := Blockchain{lastHash, db}
+	return &blockchain
 }
 
