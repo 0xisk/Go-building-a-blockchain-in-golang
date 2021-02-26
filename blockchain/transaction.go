4d7c9db1c3ad10132778e663695501614f4aecbc
iskanderandrew <iskander.s.andrews@gmail.com>
2021-02-26 10:05:42 +0200

    blockchain/transaction.go: Implementing the Coinbase tx which is basically the first transaction in the genesis block

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "fmt"
+
 type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
@@ -15,4 +17,18 @@ type TxInput struct {
 	ID  []byte
 	Out int
 	Sig string
+}
+
+func CoinbaseTx(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Coins to %s", to)
+	}
+
+	txin := TxInput{[]byte{}, -1, data}
+	txout := TxOutput{100, to}
+
+	tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+	tx.SetID()
+
+	return &tx
 }
