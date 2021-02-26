3ff88049a35e21babb3af1e35df663eb6e4114b1
iskanderandrew <iskander.s.andrews@gmail.com>
2021-02-26 10:29:29 +0200

    blockchain/block.go: Implement the HashTransaction func to loop through all the txs within the block to hash it together

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,6 +2,7 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
 	"log"
 )
@@ -19,6 +20,18 @@ type Block struct {
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }*/
+
+func (b *Block) HashTransactions() []byte {
+	var txHashes [][]byte
+	var txHash [32]byte
+
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.ID)
+	}
+	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
+
+	return txHash[:]
+}
 
 func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
 	block := &Block{[]byte{}, txs, prevHash, 0}
